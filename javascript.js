68ef3e1622c46f5a9db358bda2aa4eeb6d3ef4be
hak <hak@tutao.de>
2024-10-07 11:35:05 +0200

    add win & lose function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 let randomValue = Math.floor(Math.random() * 3) + 1;
-
-function getComputerChoice() {
+function getComputerChoice(choice) {
   if (randomValue === 1) {
     return "rock";
   } else if (randomValue === 2) {
@@ -8,11 +7,51 @@ function getComputerChoice() {
   } else {
     return "paper";
   }
-};
-
+}
 console.log(getComputerChoice());
 
-function getHumanChoice() {}
+let humanChoice = prompt("What is your choice?");
+function getHumanChoice() {
+  if (humanChoice.toLowerCase() === "rock") {
+    return "rock";
+  } else if (humanChoice.toLowerCase() === "scissors") {
+    return "scissors";
+  } else {
+    return "paper";
+  }
+}
+console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It’s a tie!");
+  } else if (
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "scissors")
+  ) {
+    console.log("you win!");
+  } else if (
+    (humanChoice === "scissors" && computerChoice === "rock") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "rock" && computerChoice === "paper")
+  )
+    console.log("you lose!");
+};
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+//1) Computer provide a random number that is the value to display: rock =1, scissors=2, paper=3
+//2) Human provide the answer: "rock" "scissors" "paper" and the answer should be checked if "rock" then 1, if scissors then 2, if paper then 3.
+//3)
+
+// you win!: scissors > paper, paper > rock, rock > scissors
+// you lose!: scissors < rock, paper < scissors, rock < paper
