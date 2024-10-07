f2ddfd83dd3a981b30abb303ff9bb5c00f1fc06b
hak <hak@tutao.de>
2024-10-07 12:02:09 +0200

    add function playRound

--- javascript.js
+++ javascript.js
@@ -26,32 +26,36 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
+  if (humanChoice.toLowerCase() === computerChoice) {
     console.log("It’s a tie!");
   } else if (
-    (humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "rock" && computerChoice === "scissors")
+    (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") ||
+    (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") ||
+    (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors")
   ) {
-    console.log("you win!");
+    console.log("You win!" + `${humanChoice.charAt(0).toUpperCase()+ humanChoice.slice(1).toLowerCase()} beats ${computerChoice.charAt(0).toUpperCase()+ computerChoice.slice(1).toLowerCase()}`);
   } else if (
-    (humanChoice === "scissors" && computerChoice === "rock") ||
-    (humanChoice === "paper" && computerChoice === "scissors") ||
-    (humanChoice === "rock" && computerChoice === "paper")
+    (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock") ||
+    (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors") ||
+    (humanChoice.toLowerCase() === "rock" && computerChoice === "paper")
   )
-    console.log("you lose!");
+  console.log("You lose!" + `${computerChoice.charAt(0).toUpperCase()+ computerChoice.slice(1).toLowerCase()} beats ${humanChoice.charAt(0).toUpperCase()+ humanChoice.slice(1).toLowerCase()}`);
 };
 
-
+function playGame
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
 
+
+
 //1) Computer provide a random number that is the value to display: rock =1, scissors=2, paper=3
 //2) Human provide the answer: "rock" "scissors" "paper" and the answer should be checked if "rock" then 1, if scissors then 2, if paper then 3.
-//3)
+//3) If you win! * 5 > you lose! * 5 return 
 
-// you win!: scissors > paper, paper > rock, rock > scissors
+// you win!: scissors > paper, paper > rock, rock > scissors . ..... beats .... 
 // you lose!: scissors < rock, paper < scissors, rock < paper
+
+
